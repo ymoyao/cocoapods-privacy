73e3cd72cc81032485a17abd84eb682ae2e93074
youhui <youhui@babybus.com>
2024-03-22 11:43:13 +0800

    优化黑白名单规则

--- lib/cocoapods-privacy/privacy/privacy_specification_hook.rb
+++ lib/cocoapods-privacy/privacy/privacy_specification_hook.rb
@@ -33,8 +33,15 @@ module Pod
 
             # 判断域名白名单 和 黑名单，确保该组件是自己的组件，第三方sdk不做检索
             config = Privacy::Config.instance          
-            git_source_whitelisted = config.source_white_list.any? { |item| git_source.include?(item) }
+
+            ## 规则：
+            ## 1、白名单/黑名单是通过组件podspec 中 source 字段的值来匹配，包含关键词即为命中，所有可以是git关键的域名，也可以是完整的git链接
+            ## 2、白名单：当白名单为空数组时：默认为全部组件都为白名单！！！； 当白名单不为空时，仅检索白名单数组内的组件
+            git_source_whitelisted = config.source_white_list.empty? ? true : config.source_white_list.any? { |item| git_source.include?(item) }
+
+            ## 3、黑名单：在白名单基础上，需要排除的组件
             git_source_blacklisted = config.source_black_list.any? { |item| git_source.include?(item) }
+            ## 4、最终检索的范围：白名单 - 黑名单
             git_source_whitelisted && !git_source_blacklisted
         end
 
